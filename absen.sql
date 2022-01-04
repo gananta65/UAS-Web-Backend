5f00ffc022d9dfe5c0e96ff477d7f59943f1e0db
ekaaditya77 <ekatsukasa@gmail.com>
2022-01-04 20:46:07 +0800

    database

---
+++ absen.sql
@@ -0,0 +1,73 @@
+/*
+SQLyog Ultimate v10.42 
+MySQL - 5.5.5-10.4.11-MariaDB : Database - absen
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`absen` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `absen`;
+
+/*Table structure for table `absensi` */
+
+DROP TABLE IF EXISTS `absensi`;
+
+CREATE TABLE `absensi` (
+  `id_absensi` int(11) NOT NULL AUTO_INCREMENT,
+  `id_karyawan` int(11) NOT NULL,
+  `tanggal` date DEFAULT NULL,
+  `jam` time DEFAULT NULL,
+  `reason` varchar(20) DEFAULT NULL,
+  `status` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id_absensi`),
+  KEY `id_karyawan` (`id_karyawan`),
+  CONSTRAINT `absensi_ibfk_1` FOREIGN KEY (`id_karyawan`) REFERENCES `karyawan` (`id_karyawan`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `absensi` */
+
+insert  into `absensi`(`id_absensi`,`id_karyawan`,`tanggal`,`jam`,`reason`,`status`) values (1,1,'2022-01-03','09:00:00','ON TIME','H'),(2,2,'2022-01-03','00:20:22','ON TIME','H');
+
+/*Table structure for table `admin` */
+
+DROP TABLE IF EXISTS `admin`;
+
+CREATE TABLE `admin` (
+  `id_admin` int(10) NOT NULL,
+  `nama_admin` varchar(50) DEFAULT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id_admin`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `admin` */
+
+insert  into `admin`(`id_admin`,`nama_admin`,`username`,`password`) values (1,'merlin','@merlin','123'),(2,'eka','@eka','1222'),(3,'karya','@tensu','4455'),(4,'endy','@endy','4459');
+
+/*Table structure for table `karyawan` */
+
+DROP TABLE IF EXISTS `karyawan`;
+
+CREATE TABLE `karyawan` (
+  `id_karyawan` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_karyawan` varchar(50) DEFAULT NULL,
+  `shift` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id_karyawan`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `karyawan` */
+
+insert  into `karyawan`(`id_karyawan`,`nama_karyawan`,`shift`) values (1,'naruto','malam'),(2,'sasuke','malam');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
